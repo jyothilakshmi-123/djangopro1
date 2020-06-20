1d8a257c123a20b38d4a06e9f91f1dc91365f7f8
Jyothilakshmi C <jyothilakshmi1491@gmail.com>
2020-06-20 18:20:21 +0530

    first commit

---
+++ examplepro/calc1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Calc1Config(AppConfig):
+    name = 'calc1'
---
+++ examplepro/calc1/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html',{'name':'jyothi'})
+
+
+def add(request):
+
+    var1 = int(request.GET['num1'])
+    var2 = int(request.GET['num2'])
+    res = var1 + var2
+
+    return render(request,"result.html",{'result':res})
+
